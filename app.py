6d63d7b87fa7938b4e4e0aec5d1a8f1e221f1995
atulpruthi <pruthi.atul@gmail.com>
2025-09-14 21:56:18 +0530

    microsoft/swin-base-patch4-window7-224

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import io
 face_detector = pipeline("object-detection", model="facebook/detr-resnet-50")
 human_checker = pipeline("image-classification", model="microsoft/beit-base-patch16-224-pt22k")
 nsfw_detector = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
-quality_checker = pipeline("image-classification", model="caidas/BeitForImageClassification")
+quality_checker = pipeline("image-classification", model="microsoft/swin-base-patch4-window7-224")
 deepfake_detector = pipeline("image-classification", model="dima806/deepfake_vs_real_image_detection")
 
 # -----------------------------
